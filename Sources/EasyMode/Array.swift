7cbc7a6d264a61e33dd3c7f9d414040f74e0abc2
Alexander Ross <alex@aross.se>
2019-10-11 17:28:51 +0200

    Add Array.grouped(byNumberOfElements:)

---
+++ Sources/EasyMode/Array.swift
@@ -0,0 +1,16 @@
+public extension Array {
+    func grouped(byNumberOfElements numberOfElements: Int) -> [[Element]] {
+        var newArray = [[Element]]()
+        var start = 0
+        let end: () -> Int = { [start + numberOfElements, self.endIndex].min()! }
+
+        while self[start..<end()].count > 0 {
+            let slice = self[start..<end()].map { $0 }
+            newArray.append(slice)
+
+            start = [end(), endIndex].min()!
+        }
+
+        return newArray
+    }
+}
---
+++ Tests/EasyModeTests/ArrayTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import EasyMode
+
+final class ArrayTests: XCTestCase {
+    func testGroupedByNumberOfElements() {
+        let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        let expected = [[1, 2, 3, 4], [5, 6, 7, 8], [9]]
+        
+        XCTAssertEqual(expected, array.grouped(byNumberOfElements: 4))
+    }
+
+    static var allTests = [
+        ("testGroupedByNumberOfElements", testGroupedByNumberOfElements),
+    ]
+}
